d20a66ce8b6b29cecbdc04419d3f954477447c87
Mitalipatel22 <vamjamitali2824@gmail.com>
2022-07-17 13:12:36 +0530

    update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
-
-
-
-ReactDOM.render(<App />, document.getElementById("root"));
+import React, { useState } from 'react' 
+ import '../styles/App.css'; 
+ import star from '../star.png' 
+ const App = () => { 
+   const [size, setSize] = useState({ height: 300, width: 300 }) 
+   const ClickOnce = () => { 
+     setSize(prev => { 
+       return { 
+         height: prev.height + 2, 
+         width: prev.width + 2 
+       } 
+     }) 
+   } 
+   return ( 
+     <div id="main" onClick={ClickOnce}> 
+       <img src={star} height={`${size.height}px`} width={`${size.width}px`} /> 
+     </div> 
+   ) 
+ } 
